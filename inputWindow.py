94a32f82c04e9865c284d095b49f9ba2681149ba
Bogdan <bogdan.mihai.timofte@gmail.com>
2016-02-14 20:18:25 +0200

    minor refactoring

--- inputWindow.py
+++ inputWindow.py
@@ -8,10 +8,12 @@ from largeType import LargeType
 class InputWindow(object):
 	def __init__(self):
 		self.screen = pygame.display.set_mode((320,240))
-		word = self.ask(self.screen)
+		word = self.ask()
 		pygame.display.quit()
-		self.lt = LargeType(word)
-		self.lt.render_text_on_screen()
+		if len(word) > 1:
+			self.lt = LargeType(word)
+			self.lt.render_text_on_screen()
+
 
 	def get_key(self):
 		while 1:
@@ -21,24 +23,24 @@ class InputWindow(object):
 			else:
 				pass
 
-	def display_box(self, screen, message):
+	def display_box(self, message):
 		"Print a message in a box in the middle of the screen"
 		fontobject = pygame.font.Font(None,18)
-		pygame.draw.rect(screen, (0,0,0), ((screen.get_width() / 2) - 100, (screen.get_height() / 2) - 10, 200,20), 0)
-		pygame.draw.rect(screen, (255,255,255),
-                   ((screen.get_width() / 2) - 102,
-                    (screen.get_height() / 2) - 12,
+		pygame.draw.rect(self.screen, (0,0,0), ((self.screen.get_width() / 2) - 100, (self.screen.get_height() / 2) - 10, 200,20), 0)
+		pygame.draw.rect(self.screen, (255,255,255),
+                   ((self.screen.get_width() / 2) - 102,
+                    (self.screen.get_height() / 2) - 12,
                     204,24), 1)
 		if len(message) != 0:
-			screen.blit(fontobject.render(message, 1, (255,255,255)),((screen.get_width() / 2) - 100, (screen.get_height() / 2) - 10))
+			self.screen.blit(fontobject.render(message, 1, (255,255,255)),((self.screen.get_width() / 2) - 100, (self.screen.get_height() / 2) - 10))
 
 		pygame.display.flip()
 
-	def ask(self, screen):
+	def ask(self):
 		"ask(screen, question) -> answer"
 		pygame.font.init()
 		current_string = []
-		self.display_box(screen, string.join(current_string,""))
+		self.display_box(string.join(current_string,""))
 		while 1:
 			inkey = self.get_key()
 			if inkey == K_BACKSPACE:
@@ -50,10 +52,8 @@ class InputWindow(object):
 			elif inkey <= 127:
 				current_string.append(chr(inkey))
 			
-			self.display_box(screen, string.join(current_string,""))
+			self.display_box(string.join(current_string,""))
 		return string.join(current_string,"")
-
-			# self.lt = LargeType(word)
 
 
 if __name__ == '__main__':
